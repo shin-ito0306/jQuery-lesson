6bce3af30b2804378fa26f2fac73bdd1f2b9b491
shin.ito <shin.20000306@icloud.com>
2021-05-20 10:40:42 +0900

    [finish] javascript-5

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery</h1>');
+$(function(){
+  $('.box-1').slideDown(1000, function (){
+    $('.box-1').css({
+      'background-color': '#0000ff',
+      'height': '100px'
+    }).slideUp(1000);
+  });
+  
 });
+
